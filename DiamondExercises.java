7f99b0b0fb4d26daca4b0cd57417cb755f1508ac
Adam <s3284176@student.rmit.edu.au>
2015-04-11 23:42:46 +1000

    Diamond class completed

--- DiamondExercises.java
+++ DiamondExercises.java
@@ -3,14 +3,20 @@ public class DiamondExercises
 
 	public String createIsoscelesTriangle(int size)
 	{
+		String triangle = createIsoscelesWithoutLastLine(size);
+		String middle = createHorizontalLine((size - 1) * 2 + 1);
+		return triangle + middle;
+	}
+
+	public String createIsoscelesWithoutLastLine(int size)
+	{
 		String triangle = "";
 
-		for (int currTier = 1; currTier <= size; currTier++)
+		for (int currTier = 1; currTier < size; currTier++)
 		{
-			triangle += createSpaces(size - currTier);
-			triangle += createHorizontalLine(currTier * 2 - 1);
-			triangle += createSpaces(size - currTier);
-			triangle += "\n";
+			int numSpaces = size - currTier;
+			int numAsterisks = currTier * 2 - 1;
+			triangle += createMixedLine(numSpaces, numAsterisks) + "\n";
 		}
 		return triangle;
 	}
@@ -18,63 +24,25 @@ public class DiamondExercises
 	public String createDiamond(int size)
 	{
 		String top = "", bottom = "", middle = "";
-		for (int currTier = 1; currTier < size; currTier++)
-		{
-			int numSpaces = size - currTier;
-			int numAsterisks = currTier * 2 - 1;
-			top += createMixedLine(numSpaces, numAsterisks) + "\n";
-		}
+		top = createIsoscelesWithoutLastLine(size);
 		middle = createHorizontalLine((size - 1) * 2 + 1);
-		bottom = new StringBuilder(top).reverse().toString();
-		return top + middle + bottom;
-	}
-	public String createDiamondWithName(int size,String name)
-	{
-		String top = "", bottom = "", middle = "";
-		for (int currTier = 1; currTier < size; currTier++)
-		{
-			int numSpaces = size - currTier;
-			int numAsterisks = currTier * 2 - 1;
-			top += createMixedLine(numSpaces, numAsterisks) + "\n";
-		}
-		middle = name;
-		bottom = new StringBuilder(top).reverse().toString();
+		bottom = invertTriangle(top);
 		return top + middle + bottom;
 	}
 
-//	public String createInvertedIsoscelesTriangle(int size)
-//	{
-//		String triangle = "";
-//		for (int currTier = size - 2; currTier >= 0; currTier--)
-//		{
-//			int numSpaces = size - currTier;
-//			int numAsterisks = currTier * 2 + 1;
-//			triangle += createMixedLine(numSpaces, numAsterisks) + "\n";
-//		}
-//		return triangle;
-//	}
-//
-//	public String createDiamond(int size)
-//	{
-//		String diamond = "";
-//		diamond += createIsoscelesTriangle(size);
-//		diamond += createInvertedIsoscelesTriangle(size - 1);
-//		return diamond;
-//	}
-//
-//	public String createDiamondWithName(int size, String name)
-//	{
-//		String diamond = "";
-//		diamond += createIsoscelesTriangle(size);
-//		diamond += name + "\n";
-//		for (int currTier = size - 2; currTier >= 0; currTier--)
-//		{
-//			int numSpaces = size - currTier;
-//			int numAsterisks = currTier * 2 + 1;
-//			diamond += createMixedLine(numSpaces, numAsterisks) + "\n";
-//		}
-//		return diamond;
-//	}
+	public String createDiamondWithName(int size, String name)
+	{
+		String top = "", bottom = "", middle = "";
+		top = createIsoscelesWithoutLastLine(size);
+		middle = name;
+		bottom = invertTriangle(top);
+		return top + middle + bottom;
+	}
+
+	public String invertTriangle(String triangle)
+	{
+		return new StringBuilder(triangle).reverse().toString();
+	}
 
 	public String createMixedLine(int spaces, int asterisks)
 	{
--- ExerciseController.java
+++ ExerciseController.java
@@ -2,8 +2,8 @@ public class ExerciseController
 {
 	public static void main(String args[])
 	{
-	//	System.out.println(new DiamondExercises().createIsoscelesTriangle(3));
-	//	System.out.println(new DiamondExercises().createDiamond(3));
+		System.out.println(new DiamondExercises().createIsoscelesTriangle(3));
+		System.out.println(new DiamondExercises().createDiamond(3));
 	//	System.out.println(new DiamondExercises().createDiamondWithName(3,"Claire"));
 		
 		
