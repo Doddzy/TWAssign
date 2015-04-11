3ac28edaadd3368ee9c7df867a20c7c909ce927c
Adam <s3284176@student.rmit.edu.au>
2015-04-11 23:03:54 +1000

    fixed diamond method

--- DiamondExercises.java
+++ DiamondExercises.java
@@ -16,6 +16,26 @@ public class DiamondExercises
 
 	}
 
+	public String createDiamond(int size)
+	{
+		String diamond = "";
+		diamond += createIsoscelesTriangle(size);
+		for (int currTier = size - 2; currTier >= 0; currTier--)
+		{
+			diamond+= createMixedLine(size-currTier,currTier*2 +1)+"\n";
+		}
+		return diamond;
+	}
+	
+	public String createMixedLine(int spaces,int asterisks)
+	{
+		String line="";
+		line += createSpaces(spaces);
+		line += createHorizontalLine(asterisks);
+		line += createSpaces(spaces);
+		return line;
+	}
+
 	public String createSpaces(int length)
 	{
 		String spaces = "";
--- ExerciseController.java
+++ ExerciseController.java
@@ -2,6 +2,7 @@ public class ExerciseController
 {
 	public static void main(String args[])
 	{
-		System.out.println(new DiamondExercises().createIsoscelesTriangle(5));
+		System.out.println(new DiamondExercises().createIsoscelesTriangle(3));
+		System.out.println(new DiamondExercises().createDiamond(3));
 	}
 }
